ac7326ded2ff190019e15c4771565f1235411023
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-31 12:56:35 -0700

    Added new trarading cards!

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,6 +1,10 @@
 class Homepage extends React.Component {
 	render(){
-		return "This component needs to be implemented.";
+		return <div>
+        <h2>Welcome user!</h2>
+        <img src="/static/img/balloonicorn.jpg" /> 
+        <h2><a href="/cards">Trading Cards!</a></h2>
+        </div>
 	}
 }
 
--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -4,7 +4,7 @@ class TradingCard extends React.Component {
 			<div className="card">
 				<h2>Name: {this.props.name}</h2>
 				<img src={this.props.imgUrl} />
-				<h2>Skill: </h2>
+				<h2>Skill: {this.props.skill}</h2>
 			</div>
 		);
 	}
@@ -16,11 +16,21 @@ ReactDOM.render(
 );
 
 ReactDOM.render(
-	<TradingCard name="Float" skill="baking pretzels" imgUrl="/static/img/float.jpg" />,
+	<TradingCard name="Float" skill="sychronized swimming" imgUrl="/static/img/float.jpg" />,
 	document.getElementById('float')
 );
 
 ReactDOM.render(
 	<TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
 	document.getElementById('llambda')
+);
+
+ReactDOM.render(
+	<TradingCard name="Seedy" skill="seed spitting contest winner" imgUrl="/static/img/seedpy.jpg" />,
+	document.getElementById('seedy')
+);
+
+ReactDOM.render(
+	<TradingCard name="Shortstack" skill="butter and syrup connoisseur" imgUrl="/static/img/shortstack-overflow.jpg" />,
+	document.getElementById('shortstack')
 );
